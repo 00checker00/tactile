e8dd88415ebe60cdc0af8024fa6af1bf9d9a8225
Sebastian Bast <s19d7a@umwelt-campus.de>
2020-07-13 01:26:34 +0200

    JSON Workflow für Motive
    Für die Informationen zu den Motiven (Name, Steine, Farben, Level) wurde eine exemplarische JSON-Datei erstellt und ein funktionierender Workflow inkl. Auslesen der Datei umgesetzt.

---
+++ AR_Game/Assets/Scripts/JSONReader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+     
+public class JSONReader : MonoBehaviour
+{
+    public TextAsset jsonFile; 
+ 
+    void Start()
+    {
+        jsonFile = Resources.Load<TextAsset> ("motives");
+        Motives motivesInJson = JsonUtility.FromJson<Motives>(jsonFile.text);
+        Debug.Log("motivesInJson: " + motivesInJson);
+        foreach (Motive motive in motivesInJson.motives) {
+            Debug.Log("Found motive: " + motive.name + ", level: " + motive.level + ", cell: " + motive.cells);
+            foreach(Cell cell in motive.cells) {
+                Debug.Log("Found cell id: " + cell.id);
+                foreach(Cellcolor color in cell.cellcolor) {
+                    Debug.Log("Color r: " + color.r);
+                    Debug.Log("Color g: " + color.g);
+                    Debug.Log("Color b: " + color.b);
+                }
+            }
+        }
+    }
+}
+
---
+++ AR_Game/Assets/Scripts/Motives.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class Motives {
+    public List<Motive> motives;
+}
+
+[System.Serializable]
+public class Cellcolor {
+    public int r;
+    public int g;
+    public int b;
+}
+
+[System.Serializable]
+public class Cell {
+    public string id;
+    public List<Cellcolor> cellcolor;
+}
+
+[System.Serializable]
+public class Motive {
+    public string name;
+    public int level;
+    public List<Cell> cells;
+}
