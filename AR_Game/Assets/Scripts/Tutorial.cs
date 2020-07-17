13f7a992db43783e0b42c3e5cb7c164b866b6152
Melissa Zaczyk <s19e08@umwelt-campus.de>
2020-07-17 16:16:11 +0200

    Grobes Grundgeruest fuer Tutorial

--- AR_Game/Assets/Scripts/Tutorial.cs
+++ AR_Game/Assets/Scripts/Tutorial.cs
@@ -1,18 +1,115 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Tutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Space(10)]
+    [Header("Introduction")]
+    public List<GameObject> introductionText = new List<GameObject>();
+    [Space(10)]
+    public float textDisplayTime = 4;
+
+    [Space(10)]
+    [Header("Explaining Pinch")]
+    public GameObject pinchText;
+    public GameObject pinchSprite;
+    public float pinchDisplayTime = 10;
+
+    // TODO: Sprite animation for pinch
+
+    [Space(10)]
+    [Header("Explaining Click")]
+    public GameObject clickText;
+    public GameObject clickSprite;
+    public float clickDisplayTime = 10;
+
+    // TODO: Sprite animation for click
+
+    // TODO: Gesture for reset/undo?
+
+    [Space(10)]
+    [Header("Loading Game")]
+    public Object gameScene;
+
+    // TODO: Button to skip tutorial
+
+    private void Start()
     {
-        
+        // TODO: Deactivate all tutorial items first
+        StartCoroutine(StartIntroduction());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator StartIntroduction()
     {
-        
+        int countIntroText = 0;
+
+        // Loading introduction text elements one by one.
+        foreach (GameObject obj in introductionText)
+        {
+            obj.SetActive(true);
+            countIntroText++;
+
+            yield return new WaitForSeconds(textDisplayTime);
+
+            // TODO: Adjust wait time after welcome
+        }
+
+        // All introduction text has been displayed. Hiding text now.
+        if (countIntroText == introductionText.Count)
+        {
+            foreach (GameObject obj in introductionText)
+            {
+                obj.SetActive(false);
+            }
+
+            StartCoroutine(ExplainPinch());
+
+            // TODO: Add wait time after every level of the tutorial
+        }
+    }
+
+    private IEnumerator ExplainPinch()
+    {
+        Debug.Log("Explaining pinch.");
+        pinchText.SetActive(true);
+        pinchSprite.SetActive(true);
+
+        yield return new WaitForSeconds(pinchDisplayTime);
+
+        pinchText.SetActive(false);
+        pinchSprite.SetActive(false);
+
+        StartCoroutine(ExplainClick());
+    }
+
+    private IEnumerator ExplainClick()
+    {
+        Debug.Log("Explaining click.");
+        clickText.SetActive(true);
+        clickSprite.SetActive(true);
+
+        yield return new WaitForSeconds(clickDisplayTime);
+
+        clickText.SetActive(false);
+        clickSprite.SetActive(false);
+
+        StartCoroutine(EndTutorial());
+    }
+
+    // End tutorial and load scene with game asynchronously in the background
+    private IEnumerator EndTutorial()
+    {
+        Debug.Log("Scene name: " + gameScene.name);
+        if (gameScene != null)
+        {
+            AsyncOperation asyncSceneLoad = SceneManager.LoadSceneAsync(gameScene.name);
+
+            while (!asyncSceneLoad.isDone)
+            {
+                yield return null;
+            }
+        }
     }
 }
